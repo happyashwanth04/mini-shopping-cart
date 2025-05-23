3825b385b8cb11ad08ee87654d4891aab9d8612a
yesh4gohan <yeshwaanth01@gmail.com>
2025-05-23 21:59:07 +0530

    PRoject and folder initialized

---
+++ backend/routes/order/order.go
@@ -0,0 +1,10 @@
+package order
+
+import "github.com/gin-gonic/gin"
+
+type OrderController struct {
+}
+
+func (oc *OrderController) CreateOrder(gc *gin.Context) {
+
+}
---
+++ backend/routes/product/product.go
@@ -0,0 +1,14 @@
+package product
+
+import "github.com/gin-gonic/gin"
+
+type ProductController struct {
+}
+
+func (pc *ProductController) GetProducts(gc *gin.Context) {
+
+}
+
+func (pc *ProductController) GetProduct(gc *gin.Context) {
+
+}
