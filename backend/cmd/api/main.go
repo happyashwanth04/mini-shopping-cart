874e859d34c264cb04aa2031126fa64f35d97900
yesh4gohan <yeshwaanth01@gmail.com>
2025-05-25 11:58:01 +0530

    Handle cors while testing locally

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -10,6 +10,17 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "content-type")
+		if c.Request.Method == "OPTIONS" {
+			c.Status(200)
+			return
+		}
+
+		c.Next()
+	})
 	productCtrl := product.NewProductController(&product_repository.ProductRepository{})
 	orderCtrl := order.NewOrderController(&product_repository.ProductRepository{})
 	r.GET("/api/product", productCtrl.GetProducts)
