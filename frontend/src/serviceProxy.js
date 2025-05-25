f2763adc16a6dfdd902347c1e984796c6a61293b
yesh4gohan <yeshwaanth01@gmail.com>
2025-05-25 14:03:52 +0530

    Added backend api base url for frontend

--- frontend/src/serviceProxy.js
+++ frontend/src/serviceProxy.js
@@ -5,7 +5,7 @@ export const makeRequest = () => {
 }
 
 const http = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
